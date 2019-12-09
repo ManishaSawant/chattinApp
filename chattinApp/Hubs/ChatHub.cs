4dc645af7e4fcf4de56f83c84132f36a2d449c85
Manisha <manisha@codeest.com>
2019-12-09 17:26:29 +0530

    commit

---
+++ chattinApp/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using chattinApp.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace chattinApp.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(Message message) =>
+            await Clients.All.SendAsync("receieve messafe",message);
+
+    }
+}
